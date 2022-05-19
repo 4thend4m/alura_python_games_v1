93ddd15ce80f77e2d4abd32836c9f085fff2e446
Kauan Soares Mota <4thend4mcc@gmail.com>
2022-05-19 01:58:45 -0300

    guessing_game_code_clean_v2

--- guessing_game.py
+++ guessing_game.py
@@ -1,18 +1,45 @@
 def play_guessing_game():
+    welcome_guessing_game_message()
 
-    from random import randint
-
-    secret_number = randint(0, 100)
-    level = 0
-    total_score = 1000
-
+    secret_number = loading_secret_number()
+    
+    attempts, missed_score, level, total_score = guessing_game_level_selector()
+    
+    total_score = guessing_game_player(attempts, missed_score, secret_number, total_score)
+    
+    final_guessing_game_message(secret_number, total_score)
+    
+    
+    
+    
+    
+    
+    
+    
+def welcome_guessing_game_message():
     print("*"*28)
     print("Welcome to the guessing game")
     print("*"*28)
 
+def level_and_score():
+    level = 0
+    total_score = 1000
+    
+    return(level, total_score)
+
+def loading_secret_number():
+    from random import randint
+    secret_number = randint(0, 100)
+
+    return(secret_number)
+
+def guessing_game_level_selector():
+    level = 0
+    total_score = 1000
+
     while(level < 1 or level > 4):
         print("1 | Easy: 20 attempts \n2 | Medium: 10 attempts\n3 | Hard: 5 attempts\n4 | Impossible: 3 attempts")
-        level = int(input("\nPut the number according to the level you want-> "))
+        level = int(input("\nPut the number according to the level you want -> "))
 
         if(level == 1):
             attempts = 20
@@ -29,7 +56,10 @@ def play_guessing_game():
         else:
             print("\n\nError, enter a valid number [1 | 2 | 3 | 4]\n\n")
             continue
-
+        
+        return(attempts, missed_score, level, total_score)
+        
+def guessing_game_player(attempts, missed_score, secret_number, total_score):
     for c in range(1, attempts+1):
 
         number = int(input("Enter a number [{0}] -> ".format(attempts)))
@@ -49,9 +79,13 @@ def play_guessing_game():
             elif (number > secret_number):
                 print("This number is greater than the secret number")
             total_score -= missed_score
-
+            
+    return(total_score)
+    
+def final_guessing_game_message(secret_number, total_score):
     print("\nThe secret number was: {}".format(secret_number))
     print("You did {} points".format(round(total_score)))
+    
 
 if(__name__ == "__main__"):
     play_guessing_game()
